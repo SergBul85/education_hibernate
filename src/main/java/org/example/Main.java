cbb63915dd0371105988901a667c4eb620753f8f
Serhii <donetsk55555@gmail.com>
2024-03-28 14:14:13 +0200

    commit 3. Insert cars to table

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,26 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+import org.example.entity.Car;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
+//        Car car1 = new Car("Mercedes", 2023, "Red", 150_000, 240.0);
+        Car car2 = new Car("BMW", 2020, "White", 175_000, 125.0);
+
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Car.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        session.beginTransaction();
+        session.save(car2);
+        session.getTransaction().commit();
+        factory.close();
 
     }
 }
