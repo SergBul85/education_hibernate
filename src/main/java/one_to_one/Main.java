bdd8d20d19d387404161fcde655ee7032b6529e1
Serhii <donetsk55555@gmail.com>
2024-04-03 22:16:34 +0300

    commit 14. One_to_one. Get and delete data from DB

--- src/main/java/one_to_one/Main.java
+++ src/main/java/one_to_one/Main.java
@@ -6,6 +6,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         SessionFactory factory = new Configuration()
@@ -17,15 +19,33 @@ public class Main {
             Session session = factory.getCurrentSession();
             session.beginTransaction();
 
+
+//            LESSON 13 ************************************************
+
 //            Car car1 = new Car("Volvo", 2020, "Black", 2000, 135.7);
 //            Engine engine1 = new Engine(500, 2000);
 //            car1.setCarEngine(engine1);
 
-            Car car1 = new Car("BMW", 2022, "White", 3000, 248.0);
-            Engine engine1 = new Engine(450, 1750);
-            car1.setCarEngine(engine1);
+//            Car car1 = new Car("BMW", 2022, "White", 3000, 248.0);
+//            Engine engine1 = new Engine(450, 1750);
+//            car1.setCarEngine(engine1);
+//            session.save(car1);
 
-            session.save(car1);
+//            LESSON 14 ************************************************
+
+//            Car car1 = session.get(Car.class,3);
+//            Engine engine = session.get(Engine.class,1);
+//            System.out.println(engine);
+
+//            List<Car> cars = session.createQuery("from Car where name='Volvo' ").getResultList();
+//            System.out.println(cars);
+
+//            Car car3 = session.get(Car.class, 3);
+//            session.delete(car3);
+
+            Engine engine3 = session.get(Engine.class, 1);
+            session.delete(engine3);
+
 
             session.getTransaction().commit();
         } finally {
